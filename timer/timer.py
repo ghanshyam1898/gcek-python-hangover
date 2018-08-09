c7fa78605763eb0b86a34afdbefffdb04167a02f
Ghanshyam Agrawal <32092151+ghanshyam1898@users.noreply.github.com>
2018-08-09 07:06:24 +0530

    Created the timer project

---
+++ timer/timer.py
@@ -0,0 +1,9 @@
+import time
+
+
+timer_time = input("Set the time for the timer in seconds : ")
+timer_time = int(timer_time)
+
+print("Timer started")
+time.sleep(timer_time)
+print("Timez up!!!!!!!")
